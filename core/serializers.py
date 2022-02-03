e3401f8b40f8921e5144597ea35d45bd32a24d5f
VAxRAxD <varadp763@gmail.com>
2022-02-03 22:34:58 +0530

    Updated API

--- core/serializers.py
+++ core/serializers.py
@@ -2,12 +2,15 @@ from rest_framework.serializers import ModelSerializer,SerializerMethodField
 from . models import *
 
 class StudentSerializer(ModelSerializer):
+    id=SerializerMethodField("get_id") 
     room_number=SerializerMethodField("get_number")
     hostel_fees=SerializerMethodField("get_hostel_fees")
     mess_fees=SerializerMethodField("get_mess_fees")
     class Meta:
         model=Student
         fields=['id','name','email','address','phone','room_number','hostel_fees','mess_fees']
+    def get_id(self,Student):
+        return str(Student.id)
     def get_number(self,Student):
         if Student.room:
             return str(Student.room.number)
